f26340aaae44800f5acbd60c85b8e5fb952251f1
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-04-29 09:16:55 +0200

    Add a ML-based classifier for important messages
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ lib/Command/TrainAccount.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @author 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\Mail\Command;
+
+use OCA\Mail\Account;
+use OCA\Mail\Exception\IncompleteSyncException;
+use OCA\Mail\Exception\ServiceException;
+use OCA\Mail\IMAP\MailboxSync;
+use OCA\Mail\Service\AccountService;
+use OCA\Mail\Service\Sync\ImapToDbSynchronizer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use function memory_get_peak_usage;
+
+class TrainAccount extends Command {
+	public const ARGUMENT_ACCOUNT_ID = 'account-id';
+
+	/** @var AccountService */
+	private $accountService;
+
+	public function __construct(AccountService $service) {
+		parent::__construct();
+
+		$this->accountService = $service;
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function configure() {
+		$this->setName('mail:account:train');
+		$this->setDescription('Train the classifier of new messages');
+		$this->addArgument(self::ARGUMENT_ACCOUNT_ID, InputArgument::REQUIRED);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$accountId = (int)$input->getArgument(self::ARGUMENT_ACCOUNT_ID);
+
+		$account = $this->accountService->findById($accountId);
+
+		$mbs = (int)(memory_get_peak_usage() / 1024 / 1024);
+		$output->writeln('<info>' . $mbs . 'MB of memory used</info>');
+	}
+
+}
